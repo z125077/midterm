16abc4ad7b059f75ceac392d009d2319bed0c90e
z125077 <z125077@shibaura-it.ac.jp>
2025-05-29 02:55:06 +0900

    -

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 500);
   }, 1500);
 });
+
+function revealOnScroll() {
+  const reveals = document.querySelectorAll('.reveal');
+  
+  for (const reveal of reveals) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveal.getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveal.classList.add('active');
+    } else {
+      reveal.classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
